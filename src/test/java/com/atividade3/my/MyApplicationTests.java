7f130925218bb8d3a9b064e9c82b82960c89342c
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-27 21:28:09 -0300

    first commit

---
+++ src/test/java/com/atividade3/my/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade3.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
