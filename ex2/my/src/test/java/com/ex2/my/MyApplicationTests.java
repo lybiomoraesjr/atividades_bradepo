cdfd52bd96416a6955955dadf73628e56ce6013d
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-28 22:16:55 -0300

    feat: add exercise 2

---
+++ ex2/my/src/test/java/com/ex2/my/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex2.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
